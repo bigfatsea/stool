defba20c8ea2e1fa01147d536a67bb4a825c962f
Stanford <bigfatsea@gmail.com>
2024-03-16 00:51:24 +0800

    x

--- stool/__main__.py
+++ stool/__main__.py
@@ -1,12 +1,12 @@
-from misc_utils import *
-from logging_utils import *
-from date_utils import *
+from .misc_utils import *
+from .logging_utils import *
+from .date_utils import *
 from concurrent.futures import ThreadPoolExecutor
 
 
 # Your main script logic using the functions from the imported modules
 if __name__ == '__main__':
-    latest_commit_time = "2024-03-16 00:45:40"
+    latest_commit_time = "2024-03-16 00:51:24"
 
     print(f'Latest commit time: {latest_commit_time}')
 
